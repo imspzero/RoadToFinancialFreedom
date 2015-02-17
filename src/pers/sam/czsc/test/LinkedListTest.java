28e9cb6c3d740659d7ab0e4fd91485c73a8e73a5
Sam.Lee <denglee123@126.com>
2015-02-17 11:11:40 +0800

    提交资源文件

---
+++ src/pers/sam/czsc/test/LinkedListTest.java
@@ -0,0 +1,37 @@
+package pers.sam.czsc.test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import pers.sam.dto.TouchDTO;
+
+/**
+ * 
+ * @author Administrator
+ *	
+ */
+public class LinkedListTest {
+	
+
+	public static void main(String args[]){
+		
+		List<Integer> list = new LinkedList<Integer>();
+		
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		
+		System.out.println(list);
+		
+		
+		
+		
+		
+	}
+
+}
